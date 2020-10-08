cff8654f47b54e4cd6d905555ed8d7eecff05986
Joel <joel.herfindal@learnet.se>
2020-10-08 14:29:28 +0200

    first commit

---
+++ Fighters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fighters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
