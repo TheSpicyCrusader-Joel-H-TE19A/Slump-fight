90f564af264fddf3d946bedf78595501a5a32ceb
Joel <joel.herfindal@learnet.se>
2020-10-09 14:10:42 +0200

    Epic :DDDD

--- Fighters/Program.cs
+++ Fighters/Program.cs
@@ -9,25 +9,34 @@ namespace Fighters
             Random Fighter1DMG = new Random();
             Random Fighter2DMG = new Random();
 
-            string Fighter1 = "BRUNO";
-            string Fighter2 = "SECCO";
+            string Fighter1 = "Jotaro";
+            string Fighter2 = "DIO";
 
-            int Fighter1HP = 150;
-            int Fighter2HP = 150;
+            int Fighter1HP = 200;
+            int Fighter2HP = 200;
+
+            Console.WriteLine($"{Fighter1}'s HP: {Fighter1HP}");
+            Console.WriteLine($"{Fighter2}'s HP: {Fighter2HP}");
+            Console.WriteLine("START!!!");
+            Console.ReadLine();
 
             while (Fighter1HP > 0 && Fighter2HP > 0)
             {
-                int DMG1 = Fighter1DMG.Next(1, 31);
-                int DMG2 = Fighter2DMG.Next(1, 31);
+                int DMG1 = Fighter1DMG.Next(1, 50);
+                int DMG2 = Fighter2DMG.Next(1, 50);
 
                 Fighter1HP = Fighter1HP - DMG2;
+                Console.WriteLine($"{Fighter2} hit {Fighter1} for {DMG2} damage.");
+
                 Fighter2HP = Fighter2HP - DMG1;
+                Console.WriteLine($"{Fighter1} hit {Fighter2} for {DMG1} damage.");
 
                 if (Fighter1HP < 0) Fighter1HP = 0;
                 if (Fighter2HP < 0) Fighter2HP = 0;
 
                 Console.WriteLine(Fighter1 + " HP: " + Fighter1HP);
                 Console.WriteLine(Fighter2 + " HP: " + Fighter2HP);
+                Console.ReadLine();
             }
 
             if (Fighter1HP > Fighter2HP)
