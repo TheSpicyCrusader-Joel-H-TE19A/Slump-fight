a350fbfae6abbb3c7c483b2471d14af00860c364
Joel <joel.herfindal@learnet.se>
2020-10-09 14:26:20 +0200

    Förklaringar

--- Fighters/Program.cs
+++ Fighters/Program.cs
@@ -20,33 +20,33 @@ namespace Fighters
             Console.WriteLine("START!!!");
             Console.ReadLine();
 
-            while (Fighter1HP > 0 && Fighter2HP > 0)
+            while (Fighter1HP > 0 && Fighter2HP > 0) // så länge dom lever, alltså deras hp är över 0, så kommer dom fortsätta att spöa skiten ur varandra
             {
-                int DMG1 = Fighter1DMG.Next(1, 50);
-                int DMG2 = Fighter2DMG.Next(1, 50);
+                int DMG1 = Fighter1DMG.Next(1, 50); //random dmg fighter 1 gör per runda
+                int DMG2 = Fighter2DMG.Next(1, 50); //random dmg fighter 2 gör per runda
 
-                Fighter1HP = Fighter1HP - DMG2;
-                Console.WriteLine($"{Fighter2} hit {Fighter1} for {DMG2} damage.");
+                Fighter1HP = Fighter1HP - DMG2; //Hur mycket hälsa fighter 1 har per runda, hur mycket dmg dom tar, och deras nya lägre hälsa infoör nästa runda
+                Console.WriteLine($"{Fighter2} hit {Fighter1} for {DMG2} damage."); //Beskriver hur mycket dmg de tar
 
-                Fighter2HP = Fighter2HP - DMG1;
-                Console.WriteLine($"{Fighter1} hit {Fighter2} for {DMG1} damage.");
+                Fighter2HP = Fighter2HP - DMG1; //Hur mycket hälsa fighter 2 har per runda, hur mycket dmg dom tar, och deras nya lägre hälsa infoör nästa runda
+                Console.WriteLine($"{Fighter1} hit {Fighter2} for {DMG1} damage."); //Beskriver hur mycket dmg de tar
 
-                if (Fighter1HP < 0) Fighter1HP = 0;
-                if (Fighter2HP < 0) Fighter2HP = 0;
+                if (Fighter1HP < 0) Fighter1HP = 0; //Om fighter 1's hp är mindre än 0 så blir det 0, för man ska inte kunna ta negativ dmg
+                if (Fighter2HP < 0) Fighter2HP = 0; //Om fighter 2's hp är mindre än 0 så blir det 0, för man ska inte kunna ta negativ dmg
 
-                Console.WriteLine(Fighter1 + " HP: " + Fighter1HP);
-                Console.WriteLine(Fighter2 + " HP: " + Fighter2HP);
-                Console.ReadLine();
+                Console.WriteLine(Fighter1 + " HP: " + Fighter1HP); //Visualiserar fighter 1's nya hälsa varje runda
+                Console.WriteLine(Fighter2 + " HP: " + Fighter2HP); //Visualiserar fighter 2's nya hälsa varje runda
+                Console.ReadLine(); //är här för att göra så att du, spectatorn, startar varja runda genom att trycka på enter knappen
             }
 
-            if (Fighter1HP > Fighter2HP)
+            if (Fighter1HP > Fighter2HP) //Om fighter 1's hp är större än fighter 2's när loopen slutar (när fighter 2's hp är 0)...
             {
-                Console.WriteLine("Fighter 1: " + Fighter1 + " WINS!");
+                Console.WriteLine("Fighter 1: " + Fighter1 + " WINS!"); //Så vinner Fighter 1
                 Console.ReadLine();
             }
-            else if (Fighter2HP > Fighter1HP)
+            else if (Fighter2HP > Fighter1HP) //Om fighter 2's hp är större än fighter 1's hp när loopen slutar (när fighter 2's hp är 0)
             {
-                Console.WriteLine("Fighter 2: " + Fighter2 + " WINS!");
+                Console.WriteLine("Fighter 2: " + Fighter2 + " WINS!"); //Så vinner Fighter 2
                 Console.ReadLine();
             }
             else
